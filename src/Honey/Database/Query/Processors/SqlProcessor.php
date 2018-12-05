aee2b9c19b7034385be98c87bea74b208591869a
Stefano Valenzano <valenzano.stefano99@gmail.com>
2018-12-05 08:52:19 +0100

    SqlDatabase fix limit

--- src/Honey/Database/Query/Processors/SqlProcessor.php
+++ src/Honey/Database/Query/Processors/SqlProcessor.php
@@ -98,7 +98,11 @@ class SqlProcessor extends Processor
 
         $where = is_array($query->wheres) ? $this->compileWheres($query) : '';
 
-        $limit = $this->compileLimit($query, $query->limit);
+        $limit = "";
+        if(isset($query->limit))
+        {
+            $limit = $this->compileLimit($query, $query->limit);
+        }
 
         $sql = trim("delete {$limit} from {$table} {$where}");
 
